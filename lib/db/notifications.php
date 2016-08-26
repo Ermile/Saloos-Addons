178812b2e24fa3a1a2983636a9ae90e306767d16
Reza Mohiti <rm.biqarar@gmail.com>
2016-08-26 12:31:58 +0430

    add notification and test github

---
+++ lib/db/notifications.php
@@ -0,0 +1,92 @@
+<?php
+namespace lib\db;
+
+/** notifications managing **/
+class notifications
+{
+	/**
+	 * this library work with notifications table
+	 * v1.0
+	 */
+
+
+	/**
+	 * insert new recrod in notifications table
+	 * @param array $_args fields data
+	 * @return mysql result
+	 */
+	public static function insert($_args){
+		
+		// creat field list string
+		$fields = join(array_keys($_args), ",");
+
+		// creat value list string
+		$values = join(array_values($_args), "','");
+
+		// make insert query
+		$query = "
+			INSERT INTO notifications
+				($fields) VALUES ('$values')
+			";
+
+		return \lib\db::query($query);
+		
+	}
+
+
+	/**
+	 * update field from notifications table
+	 * get fields and value to update
+	 * @param array $_args fields data
+	 * @param string || int $_id record id
+	 * @return mysql result
+	 */
+	public function update($_args, $_id) {
+			
+		// ready fields and values to update syntax query [update table set field = 'value' , field = 'value' , .....]
+		$query = [];
+		foreach ($_args as $field => $value) {
+			$query[] = "$field = '$value'";
+		}
+		$query = join($query, ",");
+
+		// make update query
+		$query = "
+				UPDATE notifications
+				SET $query
+				WHERE notifications.id = $_id;
+				";
+		
+		return \lib\db::query($query);
+	}
+
+
+	/**
+	 * we can not delete a record from database 
+	 * we just update field status to 'deleted' or 'disable' or set this record to black list
+	 * @param string || int $_id record id
+	 * @return mysql result
+	 */
+	public function delete($_id) {
+		// get id
+		$query = "
+				UPDATE FROM notifications
+				SET notifications.notification_status = 'expire'
+				WHERE notifications.id = $_id
+				";
+
+		return \lib\db::query($query);
+	}
+
+
+	/**
+	 * get string query and return mysql result 
+	 * @param string $_query string query
+	 * @return mysql result
+	 */
+	public function select($_query) {
+		return \lib\db::query($_query);
+	}
+
+}
+?>
