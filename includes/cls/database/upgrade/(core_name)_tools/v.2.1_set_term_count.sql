f99679406d4cbd9f3f9fe4fcd1a1d8b61027ddf4
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-03 09:54:53 +0330

    commentdetails table add`

---
+++ includes/cls/database/upgrade/(core_name)_tools/v.2.1_set_term_count.sql
@@ -0,0 +1,7 @@
+CREATE TRIGGER `set_count` AFTER INSERT ON `termusages` FOR EACH ROW BEGIN
+UPDATE terms
+SET
+	terms.term_count = IF(terms.term_count IS NULL OR terms.term_count = '', 1, terms.term_count + 1)
+WHERE
+	terms.id = NEW.term_id;
+END
