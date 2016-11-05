11c3279f7bde681de0738e980f421295edb3b8b3
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-05 14:25:36 +0330

    add comment_type field

--- includes/cls/database/upgrade/(core_name)_tools/v.3.1_comment_minus_plus_field.sql
+++ includes/cls/database/upgrade/(core_name)_tools/v.3.1_comment_minus_plus_field.sql
@@ -1,3 +1,3 @@
+ALTER TABLE `comments` ADD `comment_type` ENUM('comment', 'rate') NULL DEFAULT NULL AFTER `user_id`;
 ALTER TABLE `comments` ADD `comment_plus` int(10) unsigned NULL AFTER `user_id`;
 ALTER TABLE `comments` ADD `comment_minus` int(10) unsigned NULL AFTER `user_id`;
-ALTER TABLE `comments` ADD `comment_rate` tinyint(1) NULL AFTER `user_id`;
