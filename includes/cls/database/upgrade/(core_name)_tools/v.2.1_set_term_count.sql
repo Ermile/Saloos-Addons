9d7d570921ef795bb3c1b0cf0eacbf2a57b3398e
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-05 15:47:12 +0330

    remove tg_structure from db

--- includes/cls/database/upgrade/(core_name)_tools/v.2.1_set_term_count.sql
+++ includes/cls/database/upgrade/(core_name)_tools/v.2.1_set_term_count.sql
@@ -1,7 +1,10 @@
+DELIMITER $$
 CREATE TRIGGER `set_count` AFTER INSERT ON `termusages` FOR EACH ROW BEGIN
-UPDATE terms
+UPDATE
+	terms
 SET
 	terms.term_count = IF(terms.term_count IS NULL OR terms.term_count = '', 1, terms.term_count + 1)
 WHERE
 	terms.id = NEW.term_id;
-END
+END $$
+DELIMITER ;
