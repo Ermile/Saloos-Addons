ab9a9a6c151b5f7319c779c91b0c8dca97bbb898
Reza Mohiti <rm.biqarar@gmail.com>
2016-11-08 10:33:45 +0330

    change comments status

--- content_cp/comments/controller.php
+++ content_cp/comments/controller.php
@@ -6,6 +6,7 @@ class controller extends \addons\content_cp\home\controller
 	function _route()
 	{
 		$this->get("comments", "comments")->ALL("comments");
+		$this->post("comments")->ALL("comments");
 	}
 }
 ?>
--- content_cp/comments/model.php
+++ content_cp/comments/model.php
@@ -11,5 +11,26 @@ class model extends \addons\content_cp\home\model
 		$comments = \lib\db\comments::get_all();
 		return $comments;
 	}
+
+	public function post_comments()
+	{
+		$status     = utility::post("status");
+		$comment_id = utility::post("id");
+
+		if(($status == 'approved' || $status == 'unapproved') && $comment_id)
+		{
+			$args = ['comment_status' => $status];
+			$result = \lib\db\comments::update($args, $comment_id);
+			if($result)
+			{
+				debug::true(T_("Comment update to :status",['status' => $status]));
+			}
+			else
+			{
+				debug::error(T_("Error in update comment status"));
+			}
+
+		}
+	}
 }
 ?>
