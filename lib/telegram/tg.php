8345697cae0845fee9ca2fe63f46977a571a9ca6
baravak <itb.baravak@gmail.com>
2016-11-21 19:54:17 +0330

    telegram auto chat_id bugfix

--- lib/telegram/tg.php
+++ lib/telegram/tg.php
@@ -185,8 +185,11 @@ class tg
 			case 'editMessageText':
 			case 'editMessageCaption':
 			case 'editMessageReplyMarkup':
-				$_prop['chat_id']    = array_key_exists('chat_id', $_prop) ? $_prop['chat_id'] : self::response('chat');
-				$_prop['message_id'] = array_key_exists('message_id', $_prop) ? $_prop['message_id'] : self::response('message_id');
+				if(!isset($_prop['chat_id']) && !isset($_prop['inline_message_id']))
+				{
+					$_prop['chat_id']    = self::response('chat');
+					$_prop['message_id'] = self::response('message_id');
+				}
 				break;
 
 			case 'getUserProfilePhotos':
