fe153c5ffed8de8b7c76dcc250dc50b15f127cdb
Reza Mohiti <rm.biqarar@gmail.com>
2016-12-10 11:09:04 +0330

    update logitem_caller field

--- includes/cls/database/upgrade/(core_name)_tools/v.2.0_termusage_foreign_users.sql
+++ includes/cls/database/upgrade/(core_name)_tools/v.2.0_termusage_foreign_users.sql
@@ -4,7 +4,7 @@ ALTER TABLE `terms` ADD `term_status` ENUM('enable','disable','expired','awaitin
 ALTER TABLE `terms` ADD `term_count` INT(10) UNSIGNED NULL AFTER `term_status`;
 ALTER TABLE `logitems` ADD `logitem_type` VARCHAR(64) NULL AFTER `id`;
 
-ALTER TABLE `logitems` ADD `logitem_caller` VARCHAR(200) NOT NULL AFTER `logitem_type`;
+ALTER TABLE `logitems` ADD `logitem_caller` VARCHAR(500) NOT NULL AFTER `logitem_type`;
 
 ALTER TABLE `terms` DROP INDEX `termurl_unique`;
 ALTER TABLE `terms` ADD UNIQUE `termurl_unique` (`term_language`, `term_url`);
