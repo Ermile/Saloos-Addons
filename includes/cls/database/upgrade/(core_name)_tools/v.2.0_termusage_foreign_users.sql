fef76097187fc13e3bdb5685a33a27313031a7e7
Reza Mohiti <rm.biqarar@gmail.com>
2016-12-10 09:10:06 +0330

    update terms unique and add logitem_caller field

--- includes/cls/database/upgrade/(core_name)_tools/v.2.0_termusage_foreign_users.sql
+++ includes/cls/database/upgrade/(core_name)_tools/v.2.0_termusage_foreign_users.sql
@@ -1,5 +1,10 @@
 ALTER TABLE `termusages` CHANGE `termusage_foreign` `termusage_foreign` ENUM('posts','products','attachments','files','comments','users') CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 ALTER TABLE `terms` ADD `term_status` ENUM('enable','disable','expired','awaiting','filtered','blocked','spam','violence','pornography','other') NOT NULL DEFAULT 'awaiting' AFTER `user_id`;
-UPDATE `terms` SET `term_status` = 'enable' WHERE 1;
+
 ALTER TABLE `terms` ADD `term_count` INT(10) UNSIGNED NULL AFTER `term_status`;
 ALTER TABLE `logitems` ADD `logitem_type` VARCHAR(64) NULL AFTER `id`;
+
+ALTER TABLE `logitems` ADD `logitem_caller` VARCHAR(200) NOT NULL AFTER `logitem_type`;
+
+ALTER TABLE `terms` DROP INDEX `termurl_unique`;
+ALTER TABLE `terms` ADD UNIQUE `termurl_unique` (`term_language`, `term_url`);
