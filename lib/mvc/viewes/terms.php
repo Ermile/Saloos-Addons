18db710224b4f326174eb19ab21987fed2436aa9
Reza Mohiti <rm.biqarar@gmail.com>
2016-12-11 11:09:57 +0330

    get the term type in template

--- lib/mvc/viewes/terms.php
+++ lib/mvc/viewes/terms.php
@@ -11,25 +11,46 @@ trait terms
 	{
 		$this->data->post = array();
 		$tmp_result       = $this->model()->get_terms();
-		$tmp_fields       = array(	'id'            =>'id',
-									'term_language' =>'language',
-									'term_type'     =>'type',
-									'term_title'    =>'title',
-									'term_slug'     =>'slug',
-									'term_url'      =>'url',
-									'term_desc'     =>'desc',
-									'term_parent'   =>'parent',
-									'date_modified' =>'modified'
-								);
-		foreach ($tmp_fields as $key => $value)
-			$this->data->post[$value] = html_entity_decode($tmp_result[$key]);
+		if(!$tmp_result)
+		{
+			$tmp_result = $this->model()->get_terms_type();
+		}
 
-		$this->data->page['title'] = $this->data->post['title'];
+		$tmp_fields =
+		[
+			'id'            =>'id',
+			'term_language' =>'language',
+			'term_type'     =>'type',
+			'term_title'    =>'title',
+			'term_slug'     =>'slug',
+			'term_url'      =>'url',
+			'term_desc'     =>'desc',
+			'term_parent'   =>'parent',
+			'date_modified' =>'modified'
+		];
+
+		foreach ($tmp_fields as $key => $value)
+		{
+			if(isset($tmp_result[$key]))
+			{
+				$this->data->post[$value] = html_entity_decode($tmp_result[$key]);
+			}
+		}
+		if(isset($this->data->post['title']))
+		{
+			$this->data->page['title'] = $this->data->post['title'];
+		}
 
 		// generate datatable
 		$result = $this->model()->sp_postsInTerm();
 		$this->data->datatable = $result['result'];
 		$this->data->pagenation = $result['pagenation'];
+		if(empty($this->data->datatable))
+		{
+			$tags_list = $this->model()->get_terms_type();
+			$this->data->tags_list = $tags_list;
+		}
+
 		$this->data->datatable_cats = $this->model()->sp_catsInTerm();
 		// switch ($this->data->module)
 		// {
