e6932733a11c6c9f760385cf7313cfc5ab278acf
Reza Mohiti <rm.biqarar@gmail.com>
2016-12-17 18:40:24 +0330

    get log items by caller field

--- lib/db/logitems.php
+++ lib/db/logitems.php
@@ -15,15 +15,17 @@ class logitems
 	 * @param array $_args fields data
 	 * @return mysql result
 	 */
-	public static function insert($_args){
+	public static function insert($_args)
+	{
 
 		$set = [];
-		foreach ($_args as $key => $value) {
+		foreach ($_args as $key => $value)
+		{
 			if($value === null)
 			{
 				$set[] = " `$key` = NULL ";
 			}
-			elseif(is_int($value))
+			elseif(is_numeric($value))
 			{
 				$set[] = " `$key` = $value ";
 			}
@@ -53,11 +55,13 @@ class logitems
 	 * @param string || int $_id record id
 	 * @return mysql result
 	 */
-	public static function update($_args, $_id) {
+	public static function update($_args, $_id)
+	{
 
 		// ready fields and values to update syntax query [update table set field = 'value' , field = 'value' , .....]
 		$query = [];
-		foreach ($_args as $field => $value) {
+		foreach ($_args as $field => $value)
+		{
 			$query[] = "$field = '$value'";
 		}
 		$query = join($query, ",");
@@ -78,34 +82,100 @@ class logitems
 	 * @param string $_query string query
 	 * @return mysql result
 	 */
-	public static function select($_query , $_type = 'query') {
+	public static function select($_query , $_type = 'query')
+	{
 		return \lib\db::$_type($_query);
 	}
 
 
 	/**
-	 * Gets the logitem id by logitem title
+	 * Gets the logitem id by logitem caller
 	 *
-	 * @param      <type>  $_logitem_title  The logitem title
+	 * @param      <type>  $_caller  The logitem caller
 	 *
 	 * @return     <type>  The identifier.
 	 */
-	public static function get_id($_logitem_title)
+	public static function get_id($_caller)
+	{
+		$log_item = self::caller($_caller);
+		if($log_item && isset($log_item['id']))
+		{
+			return $log_item['id'];
+		}
+		elseif(is_numeric($log_item))
+		{
+			return $log_item;
+		}
+		return false;
+	}
+
+
+	/**
+	 * Gets the logitem record by logitem caller
+	 *
+	 * @param      <type>  $_caller  The logitem caller
+	 *
+	 * @return     <type>  The identifier.
+	 */
+	public static function caller($_caller)
 	{
 		$query =
 		"
 			SELECT
-				id
+				*
 			FROM
 				logitems
 			WHERE
-				logitems.logitem_title = '$_logitem_title'
+				logitems.logitem_caller = '$_caller'
 			LIMIT 1
 		";
-		$id = \lib\db::get($query,'id',true);
-		return $id;
+		$result = \lib\db::get($query,null,true);
+		if(!$result || empty($result))
+		{
+			return self::auto_insert($_caller);
+		}
+		return $result;
 	}
 
 
+	/**
+	 * auto insert record of logitems
+	 *
+	 * @param      <type>  $_caller  The caller
+	 *
+	 * @return     <type>  ( description_of_the_return_value )
+	 */
+	private static function auto_insert($_caller)
+	{
+		$insert_log_items = [];
+
+		// deleted account record of log
+		$insert_log_items['delete_account'] =
+		[
+			'logitem_type'     => 'users',
+			'logitem_caller'   => 'delete_account',
+			'logitem_title'    => 'delete user account',
+			'logitem_priority' => 'high'
+		];
+
+		// the account verification sms code
+		$insert_log_items['account_verification_sms'] =
+		[
+			'logitem_type'     => 'users',
+			'logitem_caller'   => 'account_verification_sms',
+			'logitem_title'    => 'verification account by sms',
+			'logitem_priority' => 'low'
+		];
+
+		if(isset($insert_log_items[$_caller]))
+		{
+			$result = self::insert($insert_log_items[$_caller]);
+			if($result)
+			{
+				return (int) \lib\db::insert_id(\lib\db::$link);
+			}
+		}
+		return false;
+	}
 }
 ?>
