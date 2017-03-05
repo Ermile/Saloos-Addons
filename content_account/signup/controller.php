5292a51fa10f6d9a7d948bc5f3ec60928c9246c0
baravak <itb.baravak@gmail.com>
2017-03-05 17:19:28 +0330

    guest signup

--- content_account/signup/controller.php
+++ content_account/signup/controller.php
@@ -5,7 +5,12 @@ class controller extends \addons\content_account\home\controller
 {
 	function _route()
 	{
-		if($this->login())
+		$temp_login = false;
+		if(isset($_SESSION['user']['mobile']) && substr($_SESSION['user']['mobile'], 0, 5) == 'temp_')
+		{
+			$temp_login = true;
+		}
+		if($this->login() && !$temp_login)
 		{
 			\lib\debug::true(T_("you are logined to system!"));
 			$this->referer();
