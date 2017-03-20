ad5b7e8484947bbb6024b54225a80eb44947dd0e
Javad <J.Evazzadeh@gmail.com>
2017-03-21 01:17:36 +0330

    allow optional redirect process for account and enter

--- lib/mvc/controller.php
+++ lib/mvc/controller.php
@@ -31,31 +31,42 @@ class controller extends \lib\controller
 			}
 			$param = $this->url('param');
 			if($param)
-				$param = '?'.$param;
-
-			switch ($this->module())
 			{
-				case 'signin':
-				case 'login':
-					$this->redirector()->set_domain($domain)->set_url(MyAccount. '/login'.$param)->redirect();
-					break;
-
-				case 'signup':
-				case 'register':
-					$this->redirector()->set_domain($domain)->set_url(MyAccount. '/signup'.$param)->redirect();
-					break;
-
-				case 'signout':
-				case 'logout':
-					// if(Domain !== MainService)
-						// $this->redirector()->set_domain(MainService.'.'.Tld)->set_url('logout')->redirect();
-					$this->redirector()->set_domain()->set_url(MyAccount. '/logout'.$param)->redirect();
-					break;
-
-				// case 'favicon.ico':
-				// 	$this->redirector()->set_domain()->set_url('static/images/favicon.png')->redirect();
-				// 	break;
+				$param = '?'.$param;
 			}
+
+			// if custom account exist, handle it, else use default login redirect process
+			if(method_exists($this, 'handle_account_url'))
+			{
+				$this->handle_account_url($this->module(), $param, $domain);
+			}
+			else
+			{
+				switch ($this->module())
+				{
+					case 'signin':
+					case 'login':
+						$this->redirector()->set_domain($domain)->set_url(MyAccount. '/login'.$param)->redirect();
+						break;
+
+					case 'signup':
+					case 'register':
+						$this->redirector()->set_domain($domain)->set_url(MyAccount. '/signup'.$param)->redirect();
+						break;
+
+					case 'signout':
+					case 'logout':
+						// if(Domain !== MainService)
+							// $this->redirector()->set_domain(MainService.'.'.Tld)->set_url('logout')->redirect();
+						$this->redirector()->set_domain()->set_url(MyAccount. '/logout'.$param)->redirect();
+						break;
+
+					// case 'favicon.ico':
+					// 	$this->redirector()->set_domain()->set_url('static/images/favicon.png')->redirect();
+					// 	break;
+				}
+			}
+
 		}
 		$myrep = router::get_repository_name();
 
