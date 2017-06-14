68715f19ee1265085e164abdb342313ab2ea1390
Reza Mohiti <rm.biqarar@gmail.com>
2017-06-15 01:12:26 +0430

    check login remmber me and set login

--- lib/mvc/controllers/login.php
+++ lib/mvc/controllers/login.php
@@ -14,17 +14,49 @@ trait login
 	{
 		if(isset($_name))
 		{
-			if($_name=="all")
+			if($_name === "all")
+			{
 				return isset($_SESSION['user'])? $_SESSION['user']: null;
-
+			}
 			else
+			{
 				return isset($_SESSION['user'][$_name])? $_SESSION['user'][$_name]: null;
+			}
 		}
 
 		if(isset($_SESSION['user']['id']))
+		{
 			return true;
+		}
 		else
+		{
 			return false;
+		}
+	}
+
+
+	/**
+	* check is set remember of this user and login by this
+	*
+	*/
+	public function check_remeber_login()
+	{
+		$url = \lib\utility\safe::safe($_SERVER['REQUEST_URI']);
+
+		// if(\lib\db\sessions::get_cookie() && !$this->login())
+		// {
+		// 	$user_id = \lib\db\sessions::get_user_id();
+
+		// 	if($user_id && is_numeric($user_id))
+		// 	{
+		// 		// set user id in static var
+		// 		self::$user_id = $user_id;
+		// 		// load user data by user id
+		// 		self::load_user_data('user_id');
+		// 		// set login session
+		// 		self::enter_set_login($url);
+		// 	}
+		// }
 	}
 }
 ?>
