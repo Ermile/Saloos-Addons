42698b953604947ca9ff005be5193d331ea14aeb
Reza <rm.biqarar@gmail.com>
2017-08-07 10:19:31 +0430

    add sorting to transaction list

--- content_cp/transactions/controller.php
+++ content_cp/transactions/controller.php
@@ -8,7 +8,25 @@ class controller extends \mvc\controller
 
 		\lib\permission::access('cp:transaction', 'block');
 
-		$this->get(false, 'list')->ALL();
+		$property                 = [];
+
+		$property['id']           = ["/.*/", true , 'id'];
+		$property['name']         = ["/.*/", true , 'name'];
+		$property['title']        = ["/.*/", true , 'title'];
+		$property['status']       = ["/.*/", true , 'status'];
+		$property['type']         = ["/.*/", true , 'type'];
+		$property['unit']         = ["/.*/", true , 'unit'];
+		$property['ninus']        = ["/.*/", true , 'ninus'];
+		$property['plus']         = ["/.*/", true , 'plus'];
+		$property['budgetbefore'] = ["/.*/", true , 'budgetbefore'];
+		$property['budget']       = ["/.*/", true , 'budget'];
+		$property['date']         = ["/.*/", true , 'date'];
+		$property['order']        = ["/.*/", true , 'order'];
+		$property['sort']         = ["/.*/", true , 'sort'];
+		$property['search']       = ["/.*/", true , 'search'];
+
+		$this->get(false, "list")->ALL(['property' => $property]);
+
 	}
 }
 ?>
--- content_cp/transactions/model.php
+++ content_cp/transactions/model.php
@@ -5,10 +5,48 @@ use \lib\utility;
 use \lib\debug;
 class model extends \mvc\model
 {
-	public function transactions_list()
+	public function transactions_list($_args)
 	{
 		$meta   = [];
-		$result = \lib\db\transactions::search(null, $meta);
+		$meta['admin'] = true;
+
+		$search = null;
+		if(isset($_args->get("search")[0]))
+		{
+			$search = $_args->get("search")[0];
+		}
+
+		if(isset($_args->get("order")[0]))
+		{
+			$meta['order'] = $_args->get("order")[0];
+		}
+
+		if(isset($_args->get("sort")[0]))
+		{
+			$meta['sort'] = $_args->get("sort")[0];
+		}
+
+		if(isset($_args->get("mobile")[0]))
+		{
+			$meta['mobile'] = $_args->get("mobile")[0];
+		}
+
+		if(isset($_args->get("caller")[0]))
+		{
+			$meta['caller'] = $_args->get("caller")[0];
+		}
+
+		if(isset($_args->get("user_id")[0]))
+		{
+			$meta['user_id'] = $_args->get("user_id")[0];
+		}
+
+		if(isset($_args->get("date")[0]))
+		{
+			$meta['date'] = $_args->get("date")[0];
+		}
+
+		$result = \lib\db\transactions::search($search, $meta);
 		return $result;
 	}
 }
